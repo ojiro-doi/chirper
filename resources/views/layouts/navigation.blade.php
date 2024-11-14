93952dceb977b57e7b326f371a0f38b344dc2aaf
ojiro-doi <sakura.0046@icloud.com>
2024-11-14 10:43:23 +0900

    ナビゲーション画面の追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Chirps') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('photos.index')" :active="request()->routeIs('photos.index')">
+                        {{ __('Photos') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -85,6 +90,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
                 {{ __('Chirps') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('photos.index')" :active="request()->routeIs('photos.index')">
+                {{ __('Photos') }}
             </x-responsive-nav-link>
         </div>
 
